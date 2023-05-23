10f0af7d229e0949904176634eea0ff38ea0b2d2
Valentin Palkovic <valentin@chromatic.com>
2023-05-23 16:33:49 +0200

    Add minimum React Application

---
+++ src/manager.tsx
@@ -0,0 +1,21 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+// Add a new DOM element to document.body, where we will bootstrap our React app
+const domNode = document.createElement("div");
+
+domNode.id = "addon-onboarding";
+domNode.style.position = "absolute";
+domNode.style.top = "0";
+domNode.style.left = "0";
+domNode.style.width = "0";
+domNode.style.height = "0";
+domNode.style.overflow = "hidden";
+domNode.style.opacity = "0";
+domNode.style.visibility = "hidden";
+
+// Append the new DOM element to document.body
+document.body.appendChild(domNode);
+
+// Render the React app
+ReactDOM.render(<div>Hello World</div>, domNode);

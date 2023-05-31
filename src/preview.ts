d86997db5528589eb9af4a0f0a9e7340e383c6be
Yann Braga <yannbf@gmail.com>
2023-05-31 14:57:18 +0200

    add missing dependencies

--- src/preview.ts
+++ src/preview.ts
@@ -1,6 +1,4 @@
 import type { Renderer, ProjectAnnotations } from "@storybook/types";
-import { ThemeProvider, ensure, themes } from "@storybook/theming";
-import React from "react";
 
 /**
  * Note: if you want to use JSX in this file, rename it to `preview.tsx`

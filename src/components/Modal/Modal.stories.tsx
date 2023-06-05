f293e4ea7e9d013f87d11fc43e33130ccaacfe6a
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-06-05 13:27:15 +0100

    Added a new Default story for Modal

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -23,6 +23,33 @@ export const Default: Story = {
     const [isOpen, setOpen] = useState(false);
     return (
       <>
+        <Modal open={isOpen}>
+          {({ Title, Description, Close }) => (
+            <>
+              Hello world
+              <Title>Modal Title</Title>
+              <Description>Modal Description</Description>
+              <Close onClick={() => setOpen(false)}>Close</Close>
+            </>
+          )}
+        </Modal>
+        <button onClick={() => setOpen(true)}>Open modal</button>
+      </>
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement.parentElement);
+    const button = canvas.getByText("Open modal");
+    await userEvent.click(button);
+    await expect(canvas.findByText("Hello world")).resolves.toBeInTheDocument();
+  },
+};
+
+export const FixedWidth: Story = {
+  render: () => {
+    const [isOpen, setOpen] = useState(false);
+    return (
+      <>
         <Modal width="740px" open={isOpen}>
           {({ Title, Description, Close }) => (
             <>

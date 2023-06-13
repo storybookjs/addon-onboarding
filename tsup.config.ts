4b3e02dfd67d417dedd42874fbc623f2362a2737
Yann Braga <yannbf@gmail.com>
2023-06-13 15:40:37 +0200

    fix dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,36 +1,35 @@
 import { defineConfig } from "tsup";
 
-const packageJson = require("./package.json");
-
-export default defineConfig((options) => ({
-  entry: ["src/index.ts", "src/preset.ts", "src/manager.tsx"],
+const baseConfig = {
   splitting: false,
-  minify: !options.watch,
-  format: ["cjs", "esm"],
   dts: {
     resolve: true,
   },
-  external: Array.from(
-    new Set([
-      ...Object.keys(packageJson.peerDependencies),
-      ...Object.keys(packageJson.dependencies),
-      "@storybook/blocks",
-      "@storybook/components",
-      "@storybook/core-events",
-      "@storybook/manager-api",
-      "@storybook/theming",
-      "@storybook/types",
-    ])
-  ),
   treeshake: true,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
-  platform: "node",
-  esbuildOptions(options) {
-    options.conditions = ["module"];
-    options.loader = {
-      ...options.loader,
-      ".png": "dataurl",
-    };
+};
+
+export default defineConfig((options) => [
+  {
+    ...baseConfig,
+    entry: ["src/preset.ts"],
+    format: ["cjs"],
+    platform: "node",
+    minify: !options.watch,
   },
-}));
+  {
+    ...baseConfig,
+    entry: ["src/index.ts", "src/manager.tsx"],
+    format: ["esm"],
+    platform: "browser",
+    esbuildOptions(options) {
+      options.conditions = ["module"];
+      options.loader = {
+        ...options.loader,
+        ".png": "dataurl",
+      };
+    },
+    minify: !options.watch,
+  },
+]);
